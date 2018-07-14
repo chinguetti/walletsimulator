ab86bf161daaa1e6266feec330f6c154f4168538
Simon Stuart <simon@mail.com>
2018-07-15 08:24:40 +0900

    xx

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:CoinFlashWeb
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def report():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run()
+    app.run(debug=True, use_reloader=True)
